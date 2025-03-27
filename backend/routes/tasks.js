42304123a4ab42b143bb77448a3e4390d7218f05
yogitagoel <yogitagoel2006@gmail.com>
2025-03-27 21:02:09 +0530

    debugging

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController.js');
+const task = require('../models/Task.js');
 
-console.log("getTasks:",getTasks);
+router.post('/tasks', async(req,res) => {
+    const task = new task({
+        title: req.body.title,
+        description: req.body.description,
+        status: req.body.status,
+        duedate: req.body.dueDate,
+    })
+    try{
+        const newtask = await task.save();
+        res.status(201).json(newtask);
+    }catch(err){
+        res.status(400).json({message:err.message});
+    }
+});
 
-router.get('/tasks', getTasks);
-router.post('/', createTask);
-router.put('/:id', updateTask);
-router.delete('/:id', deleteTask);
-
-module.exports = router;
+application.get('/tasks', async(req,res) =>{
+    try{
+        const tasks = await task.find();
+        res.json(tasks);
+    }catch(err){
+        res.status(500).json({ message: err.message });
+    }
+});
